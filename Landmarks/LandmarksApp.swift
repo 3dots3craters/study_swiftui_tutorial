e4417d936e46843f7bb31a3f52cbf539e615031d
3dots3craters <3dots3craters@gmail.com>
2022-03-23 07:19:08 +0900

    Feat: finish ch.1-1, work ch.1-2 in progress

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by hayeon on 2022/03/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
